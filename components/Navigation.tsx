4badc3d2cccac61d8719b7c9d696d87c38450bd8
David Kwong <20544dk@gmail.com>
2020-11-12 22:22:16 +1000

    posts page and navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,21 +1,63 @@
 import burgerSVG from '../media/Burger.svg'
+import darkBurgerSVG from '../media/BurgerDark.svg'
 import closeSVG from '../media/Close.svg'
+import {motion, useAnimation} from 'framer-motion'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 export default function Navigation(){
+    const router= useRouter()
+    const isHome = !router.pathname.includes("posts")
+    const menuControl = useAnimation()
+    const topBarControl =useAnimation()
+    const [menuState,setMenuState] = useState(false)
+    const handleClick = async()=>{
+      if(menuState){
+        window.scrollTo(0,0)
+        await menuControl.start({
+          opacity:0
+        })
+        await topBarControl.start({
+          opacity:1
+        })
+        setMenuState(false)
+      }else{
+        topBarControl.start({
+          opacity:0
+        })
+        await menuControl.start({
+          display:"block",
+        })
+        await menuControl.start({
+          opacity:1
+        })
+        setMenuState(true)
+      }
+    }
     return(
         <div>
-            <nav>
+            <nav className={isHome?"":"nothome"}>
                 <h6 className="hover">DAVID: BLOG</h6>
+
                 <div className="mobileMenu">
-                  <div className="menubutton hover">
-                    <img src={burgerSVG}/>
+                  <motion.div animate={topBarControl}>
+                    <div className="menubutton hover" onClick={handleClick}>
+                      <img src={isHome?burgerSVG:darkBurgerSVG}/>
+                    </div>
+                  </motion.div>
+                  <motion.div initial={{
+                    display:"none",
+                    opacity:0
+                  }} animate={menuControl}>
                     <div className="slideMenu">
                         <div className="top">
-                          <h6>Navigation</h6>
-                          <img src={closeSVG} alt="" className="menubutton"/>
+                          <img onClick={handleClick} src={closeSVG} alt="" className="menubutton"/>
                         </div>
-
+                        <ul className="navLinks">
+                            <li className="hover">HOME</li>
+                            <li className="hover">ABOUT</li>
+                        </ul>
                     </div>
-                  </div>
+                  </motion.div>
                 </div>
                 <ul className="navlinks">
                   <li className="hover">HOME</li>
@@ -23,8 +65,16 @@ export default function Navigation(){
                 </ul>
             </nav>
             <style jsx>{`
+                h6{
+                  font-weight:400;
+                }
+                .nothome{
+                  color:#2c2c2c;
+                  background:none
+                }
                 nav{
                     position:absolute;
+                    top:0;
                     color:#f6f6f6;
                     display:flex;
                     justify-content:space-between;
@@ -32,6 +82,7 @@ export default function Navigation(){
                     align-items:center;
                     padding: 1.5rem 1.2rem;
                     overflow-x:hidden;
+                    background:#2c2c2c;
                   }
                   .navlinks{
                     display:none;
@@ -39,12 +90,42 @@ export default function Navigation(){
                   }
 
                   .slideMenu{
-                    position:absolute;
+                    position:fixed;
+                    background:#2c2c2c;
+                    overflow:hidden;
+                    color:#f6f6f6;
+                    height:100vh;
+                    width:100vw;
                     top:0;
                     left:0;
                   }
+                  .slideMenu .top{
+                    position:fixed;
+                    width:100%;
+                    padding: 1.5rem 1.2rem;
+                    display:flex;
+                    flex-direction:row-reverse;
+                  }
+
+                  .slideMenu .navLinks{
+                    width:100%;
+                    height:100%;
+                    display:none;
+                    list-style:none;
+                    display:flex;
+                    flex-direction:column;
+                    justify-content:center;
+                    align-items:center;
+                  }
+                  .slideMenu .navLinks li{
+                    margin:0.5rem 0;
+                    font-size:1.5rem;
+                  }
           
                   @media(min-width:768px){
+                    .mobileMenu{
+                      display:none;
+                    }
                     .navlinks{
                       display:flex;
                     }
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,80 @@
+import { useRouter } from "next/router"
+import Post from "../../components/Post"
+import Layout from "../../layout"
+
+
+
+const PostPage = ()=>{
+
+    const router = useRouter()
+    return(
+        <Layout>
+            <div className="post">
+                <h1>THIS IS MY FIRST POST</h1>
+                <sub>IT WAS CREATED 2 WEEKS AGO, DAVID KWONG</sub>
+                <div className="imageContainer">
+                    <img src="https://images.unsplash.com/photo-1582133456397-cb5dc96e9f3e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1403&q=80" alt="Picture"/>
+                </div>
+                <p>I’m ever so thankful that God placed me on the Amos bus, crawling through the red stained outback of Australia. Through this two week episode, God has really stamped in bold the word “humility” in my mental image of serving God and evangelism. In the past, sinful pride has led me to the paradigm in which I, myself, am responsible for convincing others of the truth of the Gospel. That somehow the existence of a God we can’t even begin to fathom can be proven through rational arguments. That somehow, my knowledge of the Christian faith or my past personal experiences of God can heighten the value of my resume; as if I was a more eligible candidate for God’s mission work! </p>
+                <h2>OTHER RELEVANT POSTS</h2>
+                <Post/>
+                <Post/>
+            </div>
+            
+            <style jsx>{`
+                h2{
+                    margin-top:6rem;
+                    margin-bottom:2.8rem;
+                }
+                img{
+                    width:100%;
+                    height:100%;
+                    object-fit:cover;
+                    object-position:center;
+                }
+                sub{
+                    margin-top:1.5rem;
+                    padding-left:5px;
+                }
+                .imageContainer{
+                    margin-top:4.65rem;
+                    width:100%;
+                    height:18rem;
+                    background:#f9f9f9;
+                    overflow:hidden;
+                    border-radius:0.3rem;
+                    margin-bottom:3rem;
+                }
+                .post{
+                    color:#2c2c2c;
+                    margin:10rem 0;
+                    padding: 0 2rem;
+                    display:flex;
+                    flex-direction:column;
+                    width:100%;
+                }
+                
+                @media (min-width:768px){
+                    .post{
+                        padding: 0 4rem;
+                    }
+                    .imageContainer{
+                        height:26rem;
+                    }
+                }
+                @media (min-width:992px){
+                    .post{
+                        padding: 0 8rem;
+                        width:992px;
+                    }
+                    .imageContainer{
+                        height:32rem;
+                           
+                    }
+                }
+            `}</style>
+        </Layout>
+    )
+}
+
+export default PostPage
