6f096beb773859d3e31ae4a67100403ecff80f8d
David Kwong <20544dk@gmail.com>
2021-02-04 20:37:55 +1000

    posts broken

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,138 +1,136 @@
-import { GetStaticProps } from 'next'
-import LandingSection from '../components/LandingSection'
-import PostsSection from '../components/PostsSection'
-import { PostHeading,PostHeadingResponse } from '../interfaces'
-import Layout from '../layout'  
-import {graphqlClient} from '../graphql'
-import {gql} from 'graphql-request'
-import { useEffect, useState } from 'react'
-import {motion,useAnimation} from 'framer-motion'
+import { GetStaticProps } from "next";
+import LandingSection from "../components/LandingSection";
+import PostsSection from "../components/PostsSection";
+import { PostHeading, PostHeadingResponse } from "../interfaces";
+import Layout from "../layout";
+import { graphqlClient } from "../graphql";
+import { gql } from "graphql-request";
+import { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
 
-export interface indexProps{
-  postsData: PostHeading[]
-  cursor:string
+export interface indexProps {
+  postsData: PostHeading[];
+  cursor: string;
 }
 
+export default function Home(props: indexProps) {
+  const [postHeadings, setPostHeadings] = useState(props.postsData);
+  const [cursor, setCursor] = useState(props.cursor);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [morePosts, setMorePosts] = useState(!(props.cursor === "LP"));
 
-export default function Home(props:indexProps) {
+  const loadButtonControl = useAnimation();
+  const loaderControl = useAnimation();
 
-  
-  const [postHeadings,setPostHeadings] = useState(props.postsData)
-  const [cursor,setCursor] = useState(props.cursor)
-  const [isAnimating,setIsAnimating] = useState(false)
-  const [morePosts,setMorePosts] = useState(!(props.cursor==="LP"))
-  
-  const loadButtonControl = useAnimation()
-  const loaderControl = useAnimation()
-  
   // useEffect(()=>{console.log(`cursor changed to:${cursor}`)},[cursor])
 
-  const handleLoading = async()=>{
+  const handleLoading = async () => {
     //  console.log(cursor)
 
-    if(!isAnimating){
-      setIsAnimating(true)
+    if (!isAnimating) {
+      setIsAnimating(true);
       await loadButtonControl.start({
-        opacity:0
-      })
+        opacity: 0,
+      });
       await loadButtonControl.start({
-        display:"none",
-        transition:{duration:0.1}
-      })      
+        display: "none",
+        transition: { duration: 0.1 },
+      });
       await loaderControl.start({
-        display:"block",
-        transition:{duration:0.1}
-      })
+        display: "block",
+        transition: { duration: 0.1 },
+      });
       await loaderControl.start({
-        opacity:1,
-        transition:{duration:1}
-      })
+        opacity: 1,
+        transition: { duration: 1 },
+      });
       // console.log(graphqlClient)
       try {
-        
-        const res:PostHeadingResponse = await graphqlClient.request(postQuery(1,cursor))
-        if(!res.getPostHeadingsByDate.isError){
-          setCursor(res.getPostHeadingsByDate.cursor)
-          setPostHeadings([...postHeadings,...res.getPostHeadingsByDate.posts])
+        const res: PostHeadingResponse = await graphqlClient.request(
+          postQuery(1, cursor)
+        );
+        if (!res.getPostHeadingsByDate.isError) {
+          setCursor(res.getPostHeadingsByDate.cursor);
+          setPostHeadings([
+            ...postHeadings,
+            ...res.getPostHeadingsByDate.posts,
+          ]);
           // console.log(res.getPostHeadingsByDate.cursor)
 
-          if(res.getPostHeadingsByDate.cursor!=="LP"){
+          if (res.getPostHeadingsByDate.cursor !== "LP") {
             await loaderControl.start({
-              opacity:0
-            })
+              opacity: 0,
+            });
             await loaderControl.start({
-              display:"none",
-              transition:{duration:0.1}
-            })      
+              display: "none",
+              transition: { duration: 0.1 },
+            });
             await loadButtonControl.start({
-              display:"block",
-              transition:{duration:0.1}
-            })
+              display: "block",
+              transition: { duration: 0.1 },
+            });
             await loadButtonControl.start({
-              opacity:1,
-              transition:{duration:1}
-            })
+              opacity: 1,
+              transition: { duration: 1 },
+            });
+          } else {
+            setMorePosts(false);
           }
-          else{
-            setMorePosts(false)
-          }
-        }else{
-          console.log(res.getPostHeadingsByDate.msg)
+        } else {
+          console.log(res.getPostHeadingsByDate.msg);
         }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
       // console.log(cursor)
-     
-      setIsAnimating(false)
+
+      setIsAnimating(false);
     }
-  }
-  
-  
+  };
 
   return (
     <Layout>
-      <LandingSection></LandingSection>      
-      <PostsSection headings={postHeadings}/>
-      { morePosts && <div className="loaderSection">
-        <motion.div initial={{display:"none",opacity:0}} animate={loaderControl}>
-          <div className="loader"/>
-        </motion.div>
-        <motion.div animate={loadButtonControl} onClick={handleLoading}>
-          <div className="outlineBtn lg hover">LOAD MORE</div>    
-        </motion.div>
-      </div>
-      }
+      <LandingSection></LandingSection>
+      <PostsSection headings={postHeadings} />
+      {morePosts && (
+        <div className="loaderSection">
+          <motion.div
+            initial={{ display: "none", opacity: 0 }}
+            animate={loaderControl}
+          >
+            <div className="loader" />
+          </motion.div>
+          <motion.div animate={loadButtonControl} onClick={handleLoading}>
+            <div className="outlineBtn lg hover">LOAD MORE</div>
+          </motion.div>
+        </div>
+      )}
       <div className="margin"></div>
       <style jsx>{`
-        .loaderSection{
-          display:flex;
-          justify-content:center;
-          align-items:center;
-          width:100%;
+        .loaderSection {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
           margin: 4rem 0;
-          height:6rem;
+          height: 6rem;
         }
-        .margin{
-          margin-bottom:8rem;
+        .margin {
+          margin-bottom: 8rem;
         }
       `}</style>
     </Layout>
-  )
+  );
 }
 
-
-const postQuery = (size?:number,cursor?:string)=>{
-
-  let opts = ``
-  if(size){
-    opts+=`size:${size},`
+const postQuery = (size?: number, cursor?: string) => {
+  let opts = ``;
+  if (size) {
+    opts += `size:${size},`;
   }
-  if(cursor){
-    opts+=`cursor:"${cursor}"`
+  if (cursor) {
+    opts += `cursor:"${cursor}"`;
   }
-
-
 
   return gql`
   {
@@ -148,18 +146,19 @@ const postQuery = (size?:number,cursor?:string)=>{
       }
     }
   }
-`
-}
+`;
+};
 
-export const getStaticProps:GetStaticProps = async(context)=>{
+export const getStaticProps: GetStaticProps = async (context) => {
   // console.log(graphqlClient)
-  const data:PostHeadingResponse = await graphqlClient.request(postQuery(20))
-  if (data.getPostHeadingsByDate.isError) console.log(data.getPostHeadingsByDate.msg)
-  return{
-    props:{
-      postsData:data.getPostHeadingsByDate.posts,
-      cursor:data.getPostHeadingsByDate.cursor,  
+  const data: PostHeadingResponse = await graphqlClient.request(postQuery(20));
+  if (data.getPostHeadingsByDate.isError)
+    console.log(data.getPostHeadingsByDate.msg);
+  return {
+    props: {
+      postsData: data.getPostHeadingsByDate.posts,
+      cursor: data.getPostHeadingsByDate.cursor,
     },
-    revalidate:1
-  }
-}
+    revalidate: 30,
+  };
+};
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,133 +1,127 @@
-import { useRouter } from "next/router"
-import Post from "../../components/Post"
-import Layout from "../../layout"
-import {GetStaticPaths, GetStaticProps} from 'next'
-import { graphqlClient } from "../../graphql"
-import { gql } from "graphql-request"
-import { PostDataReponseData, PostPageResponse, PostHeading, PostIdsResponse } from "../../interfaces"
-import formatDate from '../../formatDate'
+import { useRouter } from "next/router";
+import Post from "../../components/Post";
+import Layout from "../../layout";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { graphqlClient } from "../../graphql";
+import { gql } from "graphql-request";
+import {
+  PostDataReponseData,
+  PostPageResponse,
+  PostHeading,
+  PostIdsResponse,
+} from "../../interfaces";
+import formatDate from "../../formatDate";
 
-
-interface PostPageProps extends PostDataReponseData{
-    relevantPosts?:PostHeading[]
+interface PostPageProps extends PostDataReponseData {
+  relevantPosts?: PostHeading[];
 }
 
+const PostPage = (props: PostPageProps) => {
+  const link =
+    "https://images.unsplash.com/photo-1476490445914-cc8ed84a4277?ixlib=rb-1.2.1&auto=format&fit=crop&w=1866&q=80";
+  // console.log(props)
+  return (
+    <Layout>
+      <div className="post">
+        <h1>{props.title?.toUpperCase()}</h1>
+        <sub>{formatDate(props.createdAt!) + ", DAVID KWONG"}</sub>
+        <div className="imageContainer">
+          <img src={props.src ? props.src : link} alt="Picture" />
+        </div>
+        {props.para?.map((x, i) => {
+          return <p key={i}>{x}</p>;
+        })}
+        {props.relevantPosts && <h2>OTHER RELEVANT POSTS</h2>}
+        {props.relevantPosts?.map((x, i) => {
+          return <Post {...x} key={i} />;
+        })}
+      </div>
 
-const PostPage = (props:PostPageProps)=>{
-
-    const link = "https://images.unsplash.com/photo-1476490445914-cc8ed84a4277?ixlib=rb-1.2.1&auto=format&fit=crop&w=1866&q=80"
-    // console.log(props)
-    return(
-        <Layout>
-            <div className="post">
-                <h1>{props.title?.toUpperCase()}</h1>
-                <sub>{formatDate(props.createdAt!)+", DAVID KWONG"}</sub>
-                <div className="imageContainer">
-                    <img src={props.src?props.src:link} alt="Picture"/>
-                </div>
-                {props.para?.map((x,i)=>{return(
-                    <p key={i}>{x}</p>
-                )})}
-                {props.relevantPosts && <h2>OTHER RELEVANT POSTS</h2>}
-                {props.relevantPosts?.map((x,i)=>{
-                    return(
-                        <Post {...x} key={i}/>
-                    )
-                })}
-            </div>
-            
-            <style jsx>{`
-
-                h1{
-                    overflow-wrap:break-word;
-                }
-                h2{
-                    margin-top:6rem;
-                    margin-bottom:2.8rem;
-                    
-                }
-                img{
-                    width:100%;
-                    height:100%;
-                    object-fit:cover;
-                    object-position:center;
-                }
-                sub{
-                    margin-top:1.5rem;
-                    padding-left:5px;
-                }
-                p{
-                    margin-bottom:2.5rem;
-                }
-                .imageContainer{
-                    margin-top:4.65rem;
-                    width:100%;
-                    max-height:18rem;
-                    background:#f9f9f9;
-                    overflow:hidden;
-                    border-radius:0.3rem;
-                    margin-bottom:3rem;
-                }
-                .post{
-                    color:#2c2c2c;
-                    margin:10rem 0;
-                    padding: 0 2rem;
-                    display:flex;
-                    flex-direction:column;
-                    width:100%;
-                    overflow-x:hidden;
-                }
-
-                
-                @media (min-width:768px){
-                    .post{
-                        padding: 0 4rem;
-                    }
-                    .imageContainer{
-                        max-height:26rem;
-                    }
-                }
-                @media (min-width:992px){
-                    .post{
-                        padding: 0 8rem;
-                        width:992px;
-                    }
-                    .imageContainer{
-                        max-height:32rem;
-                           
-                    }
-                }
-            `}</style>
-        </Layout>
-    )
-}
-
-export default PostPage
-
-
-
-export const getStaticPaths:GetStaticPaths = async () =>{
-    const query = gql`
-        {
-            AllPostId{
-                isError
-                msg
-                ids
-            }
+      <style jsx>{`
+        h1 {
+          overflow-wrap: break-word;
         }
-    `
-    const ids:PostIdsResponse = await graphqlClient.request(query)
-    return{
-        paths:ids.AllPostId.ids!.map(x=>({params:{id:x}})),
-        fallback:true
+        h2 {
+          margin-top: 6rem;
+          margin-bottom: 2.8rem;
+        }
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          object-position: center;
+        }
+        sub {
+          margin-top: 1.5rem;
+          padding-left: 5px;
+        }
+        p {
+          margin-bottom: 2.5rem;
+        }
+        .imageContainer {
+          margin-top: 4.65rem;
+          width: 100%;
+          max-height: 18rem;
+          background: #f9f9f9;
+          overflow: hidden;
+          border-radius: 0.3rem;
+          margin-bottom: 3rem;
+        }
+        .post {
+          color: #2c2c2c;
+          margin: 10rem 0;
+          padding: 0 2rem;
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+          overflow-x: hidden;
+        }
+
+        @media (min-width: 768px) {
+          .post {
+            padding: 0 4rem;
+          }
+          .imageContainer {
+            max-height: 26rem;
+          }
+        }
+        @media (min-width: 992px) {
+          .post {
+            padding: 0 8rem;
+            width: 992px;
+          }
+          .imageContainer {
+            max-height: 32rem;
+          }
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+export default PostPage;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const query = gql`
+    {
+      AllPostId {
+        isError
+        msg
+        ids
+      }
     }
-}
+  `;
+  const ids: PostIdsResponse = await graphqlClient.request(query);
+  return {
+    paths: ids.AllPostId.ids!.map((x) => ({ params: { id: x } })),
+    fallback: true,
+  };
+};
 
-export const getStaticProps:GetStaticProps = async (context)=>{
+export const getStaticProps: GetStaticProps = async (context) => {
+  const id = `"${context.params!.id}"`;
 
-    const id = `"${context.params!.id}"`
-
-    const query = gql`
+  const query = gql`
         {
             getPostById(id:${id}){
                 isError
@@ -150,39 +144,37 @@ export const getStaticProps:GetStaticProps = async (context)=>{
                 }
             }
         }
-    `
-    try {
-        const res:PostPageResponse = await graphqlClient.request(query)
-        // console.log(res)
-        const relevantPosts:PostHeading[] =[]
-        if(!res.getPostHeadingsByDate.isError){
-            for(let x of res.getPostHeadingsByDate.posts){
-                if(x.id!==context.params!.id){
-                    relevantPosts.push(x)
-                }
-            }
+    `;
+  try {
+    const res: PostPageResponse = await graphqlClient.request(query);
+    // console.log(res)
+    const relevantPosts: PostHeading[] = [];
+    if (!res.getPostHeadingsByDate.isError) {
+      for (let x of res.getPostHeadingsByDate.posts) {
+        if (x.id !== context.params!.id) {
+          relevantPosts.push(x);
         }
-        if(!res.getPostById.isError){
-            return{
-                props:{
-                    title:res.getPostById.title || "Title Missing",
-                    description:res.getPostById.description || "Description Missing",
-                    src:res.getPostById.src,
-                    para:res.getPostById.para || ["Missing Paragraphs"],
-                    createdAt:res.getPostById.createdAt,
-                    relevantPosts:relevantPosts
-                
-                },
-                revalidate:1
-            }
-        }else{
-            throw res.getPostById.msg
-        }
-    } catch (error) {
-        console.log(error)
+      }
     }
-    return{
-        props:{}
+    if (!res.getPostById.isError) {
+      return {
+        props: {
+          title: res.getPostById.title || "Title Missing",
+          description: res.getPostById.description || "Description Missing",
+          src: res.getPostById.src,
+          para: res.getPostById.para || ["Missing Paragraphs"],
+          createdAt: res.getPostById.createdAt,
+          relevantPosts: relevantPosts,
+        },
+        revalidate: 30,
+      };
+    } else {
+      throw res.getPostById.msg;
     }
-    
-}
+  } catch (error) {
+    console.log(error);
+  }
+  return {
+    props: {},
+  };
+};
